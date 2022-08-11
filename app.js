eb7a04b42f036f2f009f1a62f534a1a8764d37b8
bavishya-vitraya <bavishya@vitrayatech.com>
2022-08-11 13:37:55 +0530

    BenefitCodes modified

--- app.js
+++ app.js
@@ -43,36 +43,40 @@ app.post('/findCoverage', (req, res) => {
 
 	let product = products[input.product_code || 'NIVABUPA_REASSURE']
 	let benefitCategory = input.benefit_category || 'INPATIENT'
-	let benefit_codes = input.benefit_codes
-
+	let benefit_codes = []
 	let result = {}
 	let coverages = []
+
+	if(input.benefit_code)
+		benefit_codes.push(input.benefit_code)
+	else
+		benefit_codes = input.benefit_codes
+
 	let total_benefits = benefit_codes.length
 	let total_benefits_covered = 0,total_benefits_excluded = 0
 	if (product) {
 		if (total_benefits) {
+			let defaultWaitingPeriod = "30 days"
+
+			for (let defaultwaitingPeriodObj of product.defaultWaitingPeriod) {
+				if(defaultwaitingPeriodObj.waitingPeriodCondition && fhirpath.evaluate(input, defaultwaitingPeriodObj.waitingPeriodCondition)[0]) {
+					defaultWaitingPeriod = defaultwaitingPeriodObj.waitingPeriod
+					break
+				}
+			}
+
+			let defaultWaitingPeriodForPEDs = "36 months"
+
+			for (let defaultwaitingPeriodPEDObj of product.defaultWaitingPeriodForPEDs) {
+				if(defaultwaitingPeriodPEDObj.waitingPeriodCondition && fhirpath.evaluate(input, defaultwaitingPeriodPEDObj.waitingPeriodCondition)[0]) {
+					defaultWaitingPeriodForPEDs = defaultwaitingPeriodPEDObj.waitingPeriod
+					break
+				}
+			}
 			for(let benefit_code of benefit_codes){
-				console.log(benefit_code)
-				let benefit_result = {}
-				let defaultWaitingPeriod = "30 days"
-
-				for (let defaultwaitingPeriodObj of product.defaultWaitingPeriod) {
-					if(defaultwaitingPeriodObj.waitingPeriodCondition && fhirpath.evaluate(input, defaultwaitingPeriodObj.waitingPeriodCondition)[0]) {
-						defaultWaitingPeriod = defaultwaitingPeriodObj.waitingPeriod
-						break
-					}
-				}
-
-				let defaultWaitingPeriodForPEDs = "36 months"
-
-				for (let defaultwaitingPeriodPEDObj of product.defaultWaitingPeriodForPEDs) {
-					if(defaultwaitingPeriodPEDObj.waitingPeriodCondition && fhirpath.evaluate(input, defaultwaitingPeriodPEDObj.waitingPeriodCondition)[0]) {
-						defaultWaitingPeriodForPEDs = defaultwaitingPeriodPEDObj.waitingPeriod
-						break
-					}
-				}
 
 				let benefit = {}
+				let benefit_result = {}
 
 				for (let benefitObj of product.benefitDetails[benefitCategory]) {
 					if (benefitObj.benefitCodes.includes(benefit_code)) {
@@ -81,6 +85,7 @@ app.post('/findCoverage', (req, res) => {
 					}
 				}
 				console.log(benefit);
+			
 				if (Object.keys(benefit).length) {
 					benefit_result["benefit_covered"] = benefit.covered
 					benefit_result["benefit_name"] = benefit.benefitName
@@ -143,22 +148,22 @@ app.post('/findCoverage', (req, res) => {
 					benefit_result["benefit_waiting_period"] = defaultWaitingPeriod
 				
 				benefit_result["benefit_default_waiting_period_for_PEDs"] = defaultWaitingPeriodForPEDs
+				
 				if(benefit_result["benefit_covered"])
 					total_benefits_covered++
 				else
 					total_benefits_excluded++
-				coverages.push(benefit_result);
-				console.log(coverages);
+				
+				if(total_benefits > 1){
+					coverages.push(benefit_result)
+					result["coverage_results"] = coverages
+					result["total_benefits_covered"] = total_benefits_covered
+					result["total_benefits_excluded"] = total_benefits_excluded
+					result["total_benefits"] = total_benefits
+				}
+				else
+					result = benefit_result
 			}
-			result["coverage_results"] = coverages
-			if(total_benefits == 1){
-				result = coverages[0];
-				result["coverage_results"] = []
-			}
-			result["total_benefits_covered"] = total_benefits_covered
-			result["total_benefits_excluded"] = total_benefits_excluded
-			result["total_benefits"] = total_benefits	
-			console.log(result);
 		}
 		else {
 			errors.push(`benefit_code is missing in the input`)
